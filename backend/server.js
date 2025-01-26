76ba3724953b8754f7c02006355525dc25c26291
yuanchengg <>
2025-01-27 00:23:12 +0800

    Change port in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const bookRoutes = require("./routes/bookRoutes");
 app.use("/users", userRoutes);
 app.use("/books", bookRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
